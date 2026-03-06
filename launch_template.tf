b7158d00d82c2e77e6fa4d20c77cae0a831777fd
olaniyi <ayodelebwj@gmail.com>
2026-03-05 21:11:25 -0500

    ghgfhgf

--- launch_template.tf
+++ launch_template.tf
@@ -8,11 +8,11 @@ resource "aws_launch_template" "frontend_lt" {
   ]
 
   user_data = <<-EOF
-      #!/bin/bash
-      sudo apt update -y
-      sudo apt install nginx -y
-      sudo systemctl start nginx
-      sudo systemctl enable nginx
+  #!/bin/bash
+  sudo apt update -y
+  sudo apt install nginx -y
+  sudo systemctl start nginx
+  sudo systemctl enable nginx
   EOF
   tag_specifications {
     resource_type = "instance"
