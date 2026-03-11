73cc7eab3cece67b5874ba73102991568b547054
olaniyi <ayodelebwj@gmail.com>
2026-03-11 17:40:12 -0400

    gvhgvh

--- alb.tf
+++ alb.tf
@@ -3,5 +3,5 @@ resource "aws_lb" "app_lb" {
   load_balancer_type = "application"
   security_groups    = [aws_security_group.alb_sg.id]
 
-  subnets = [data.aws_subnet.public.id, data.aws_subnet.public_2.id]
+  subnets = [data.aws_subnet.public_subnet.id, data.aws_subnet.public_subnet_2.id]
 }
