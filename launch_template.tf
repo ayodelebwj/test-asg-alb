fe2d0a54a75a5ddff8ba7b2233dcee31acf25cee
olaniyi <ayodelebwj@gmail.com>
2026-03-12 08:28:20 -0400

    sdcsdcsd

--- launch_template.tf
+++ launch_template.tf
@@ -39,14 +39,43 @@ aws_security_group.backend_ec2_sg.id
 
   user_data = base64encode(<<EOF
 #!/bin/bash
+# install python and dependencies
 sudo apt update -y
-sudo apt install python3.12-venv -y
-python3 -m venv venv
-source venv/bin/activate
+sudo apt install -y python3.12-venv python3-pip git
+git clone https://github.com/techbleat/class25-26-project2.git
+# create virtual environment
+cd /home/ubuntu
+python3 -m venv class25-26-project2/venv
+
+# activate venv and install requirements
+source class25-26-project2/venv/bin/activate
 cd class25-26-project2
 pip install -r requirements.txt
-uvicorn app.main:app --host 0.0.0.0 --port 8000 --reload &
+
+# create systemd service
+cat <<EOF | sudo tee /etc/systemd/system/backend-app.service
+[Unit]
+Description=Backend FastAPI App
+After=network.target
+
+[Service]
+User=ubuntu
+Group=ubuntu
+WorkingDirectory=/home/ubuntu/class25-26-project2
+Environment="PATH=/home/ubuntu/class25-26-project2/venv/bin"
+ExecStart=/home/ubuntu/class25-26-project2/venv/bin/uvicorn app.main:app --host 0.0.0.0 --port 8000
+Restart=always
+RestartSec=5
+
+[Install]
+WantedBy=multi-user.target
+
+# reload systemd and start service
+sudo systemctl daemon-reload
+sudo systemctl enable backend-app
+sudo systemctl start backend-app
 EOF
+
   )
   tag_specifications {
     resource_type = "instance"
